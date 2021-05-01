df3d73f1b7642ffdbb2a2c70b13fe99f1eebb6cb
/Developer/ <80012221+DevloperSamblox@users.noreply.github.com>
2021-04-30 23:28:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const client = new Discord.Client();
 const config = require("./config.json");
 
 
-client.login(process.env['TOKEN'])
+client.login("ODM0NTAyNDkwNDU1OTMyOTg5.YIB1Ag.4rJte-1FCVd0jNRDJev4n29BVwg")
