e82b7165ea7887735ae347e970c10079bf922487
/Developer/ <80012221+DevloperSamblox@users.noreply.github.com>
2021-04-30 23:29:08 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
-const config = require("./config.json");
 
-
-client.login("ODM0NTAyNDkwNDU1OTMyOTg5.YIB1Ag.4rJte-1FCVd0jNRDJev4n29BVwg")
